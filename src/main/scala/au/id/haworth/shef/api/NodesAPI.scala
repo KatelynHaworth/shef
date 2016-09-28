66e908ccb1c2e999d05b871320c0191bd698bcef
Liam Haworth <liam.haworth@tesserent.com>
2016-09-28 14:13:00 +1000

    Finished fleshing out the Nodes API by adding the update and delete requests

--- src/main/scala/au/id/haworth/shef/api/NodesAPI.scala
+++ src/main/scala/au/id/haworth/shef/api/NodesAPI.scala
@@ -24,9 +24,8 @@ protected[shef] abstract class NodesAPI extends ChefAPI {
     * @return A map of Name -> URL entries
     */
   def getNodeList: Future[Map[String, String]] = {
-    val nodesPipeline: (HttpRequest) => Future[Map[String, String]] = {
+    val nodesPipeline: (HttpRequest) => Future[Map[String, String]] =
       defaultPipeline ~> unmarshal[Map[String, String]]
-    }
 
     nodesPipeline(Get(s"/organizations/${chefClient.organization}/nodes"))
   }
@@ -37,7 +36,8 @@ protected[shef] abstract class NodesAPI extends ChefAPI {
     * @param node The node to register with the Chef API server
     * @return Returns the name of the node and its Chef API URL
     */
-  def createNode(node: Node): Future[HttpResponse] = defaultPipeline(Post(s"/organizations/${chefClient.organization}/nodes", node))
+  def createNode(node: Node): Future[HttpResponse] =
+    defaultPipeline(Post(s"/organizations/${chefClient.organization}/nodes", node))
 
   /**
     * Requests detail informaton about a node from
@@ -47,10 +47,36 @@ protected[shef] abstract class NodesAPI extends ChefAPI {
     * @return Returns a Node object with the details about the node
     */
   def getNode(nodeName: String): Future[Node] = {
-    val nodePipeline: (HttpRequest) => Future[Node] = {
+    val nodePipeline: (HttpRequest) => Future[Node] =
       defaultPipeline ~> unmarshal[Node]
-    }
 
     nodePipeline(Get(s"/organizations/${chefClient.organization}/nodes/$nodeName"))
+  }
+
+  /**
+    * Informs the Chef API of updates to a Nodes details
+    *
+    * @param name The name of the node
+    * @param node The node object with the changes made
+    * @return Returns a Node object with the changes implemented
+    */
+  def updateNode(name: String, node: Node): Future[Node] = {
+    val updatePipeline: (HttpRequest) => Future[Node] =
+      defaultPipeline ~> unmarshal[Node]
+
+    updatePipeline(Put(s"/organizations/${chefClient.organization}/nodes/$name"))
+  }
+
+  /**
+    * Informs the Chef API to unregister and delete the node
+    *
+    * @param name The name of the node
+    * @return Returns a Node with the settings last known by the Chef API
+    */
+  def deleteNode(name: String): Future[Node] = {
+    val deletePipeline: (HttpRequest) => Future[Node] =
+      defaultPipeline ~> unmarshal[Node]
+
+    deletePipeline(Delete(s"/organizations/${chefClient.organization}/nodes/$name"))
   }
 }
